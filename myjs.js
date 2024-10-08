d58068b3aa4fc7c1e9f8e6c8cc0e2a316488ccd0
yagiayaka <167832854+yagiayaka@users.noreply.github.com>
2024-10-08 10:06:38 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,1 +1,11 @@
 alert('Hello,Ayaka!');
+alert(1+1);
+alert(Math.sqrt(1+1));
+alert(Math.sqrt(2+3));
+alert(Math.sqrt(1/0));
+alert(Math.sqrt(0/0));
+alert(2+3);
+alert('2'+'3');
+alert(Number('2')+Number('3'));
+alert('2'+3);
+alert(Number('2')+3);
