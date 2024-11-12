a136034a7bc9117e6670ffa3fa0fae0c19cab9ff
yagiayaka <167832854+yagiayaka@users.noreply.github.com>
2024-11-12 10:03:06 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -1,5 +1,5 @@
 var who66='world';
-function greet sayhello66(){alerrt('Hello,'+who66;'!');}
-function greet taro66(){who66='太郎';}
-function greet hanako66(){who66='花子';}
-function greet gakita66(){alert(who66+'が来た!');}
+function  sayhello66(){alerrt('Hello,'+who66;'!');}
+function taro66(){who66='太郎';}
+function  hanako66(){who66='花子';}
+function  gakita66(){alert(who66+'が来た!');}
