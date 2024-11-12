6c20964da3bdf1605e65b6dcf95c0db107c603ba
yagiayaka <167832854+yagiayaka@users.noreply.github.com>
2024-11-12 22:20:21 +0900

    Update ex.6-88.js

--- ex.6-88.js
+++ ex.6-88.js
@@ -1,3 +1,3 @@
 function gazo688() {
-    document.getElementById('who688').innerHTML = '<img src="./image/.png" alt="オン">';
+    document.getElementById('who688').innerHTML = '<img src="./image/naku.png" alt="オン">';
 }
