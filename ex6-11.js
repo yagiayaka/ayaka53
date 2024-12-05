86f59a90789abc370db56c37d0ce1c4f633269cd
yagiayaka <167832854+yagiayaka@users.noreply.github.com>
2024-12-05 22:08:36 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,7 @@
-function imgchange3(){document.getElementById('logo').src='./image/jokoma.jpg';}
-function imgchange4(){document.getElementById('logo').src='./image/josai.jpg';}
+function imgchange3() {
+    document.getElementById('logo').src = './image/jokoma.jpg';
+}
+function imgchange4() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
+
